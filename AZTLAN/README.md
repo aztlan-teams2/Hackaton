70ee8108a80d566d12eee04a7152f6b4e6bb33a5
aztlan-teams2 <waachma28254first@gmail.com>
2026-04-08 15:35:27 -0600

    Add files via upload

--- AZTLAN/README.md
+++ AZTLAN/README.md
@@ -1,15 +1,15 @@
 # Installation
-> `npm install --save @types/json-schema`
+> `npm install --save @types/d3-timer`
 
 # Summary
-This package contains type definitions for json-schema (https://github.com/kriszyp/json-schema).
+This package contains type definitions for d3-timer (https://github.com/d3/d3-timer/).
 
 # Details
-Files were exported from https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/json-schema.
+Files were exported from https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/d3-timer.
 
 ### Additional Details
- * Last updated: Tue, 07 Nov 2023 03:09:37 GMT
+ * Last updated: Tue, 07 Nov 2023 15:11:37 GMT
  * Dependencies: none
 
 # Credits
-These definitions were written by [Boris Cherny](https://github.com/bcherny), [Lucian Buzzo](https://github.com/lucianbuzzo), [Roland Groza](https://github.com/rolandjitsu), and [Jason Kwok](https://github.com/JasonHK).
+These definitions were written by [Tom Wanzek](https://github.com/tomwanzek), [Alex Ford](https://github.com/gustavderdrache), [Boris Yankov](https://github.com/borisyankov), [denisname](https://github.com/denisname), and [Nathan Bierema](https://github.com/Methuselah96).
--- AZTLAN/index.d.ts
+++ AZTLAN/index.d.ts
@@ -1,316 +1,68 @@
 // Last module patch version validated against: 3.0.1
 
-// --------------------------------------------------------------------------
-// Easing Functions
-// --------------------------------------------------------------------------
-
 /**
- * Linear easing; the identity function; linear(t) returns t.
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
+ * Returns the current time as defined by performance.now if available, and Date.now if not.
+ * The current time is updated at the start of a frame; it is thus consistent during the frame, and any timers scheduled during the same frame will be synchronized.
+ * If this method is called outside of a frame, such as in response to a user event, the current time is calculated and then fixed until the next frame,
+ * again ensuring consistent timing during event handling.
  */
-export function easeLinear(normalizedTime: number): number;
+export function now(): number;
 
-/**
- * Symmetric quadratic easing; scales quadIn for t in [0, 0.5] and quadOut for t in [0.5, 1]. Also equivalent to poly.exponent(2).
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeQuad(normalizedTime: number): number;
-
-/**
- * Quadratic easing; equivalent to polyIn.exponent(2).
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeQuadIn(normalizedTime: number): number;
-
-/**
- * Reverse quadratic easing; equivalent to 1 - quadIn(1 - t). Also equivalent to polyOut.exponent(2).
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeQuadOut(normalizedTime: number): number;
-
-/**
- * Symmetric quadratic easing; scales quadIn for t in [0, 0.5] and quadOut for t in [0.5, 1]. Also equivalent to poly.exponent(2).
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeQuadInOut(normalizedTime: number): number;
-
-/**
- * Symmetric cubic easing; scales cubicIn for t in [0, 0.5] and cubicOut for t in [0.5, 1]. Also equivalent to poly.exponent(3).
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeCubic(normalizedTime: number): number;
-
-/**
- * Cubic easing; equivalent to polyIn.exponent(3).
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeCubicIn(normalizedTime: number): number;
-
-/**
- * Reverse cubic easing; equivalent to 1 - cubicIn(1 - t). Also equivalent to polyOut.exponent(3).
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeCubicOut(normalizedTime: number): number;
-
-/**
- * Symmetric cubic easing; scales cubicIn for t in [0, 0.5] and cubicOut for t in [0.5, 1]. Also equivalent to poly.exponent(3).
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeCubicInOut(normalizedTime: number): number;
-
-/**
- * Polynomial easing function factory
- */
-export interface PolynomialEasingFactory {
+export interface Timer {
     /**
-     * Calculate eased time.
-     * @param normalizedTime Normalized time typically in the range [0, 1]
-     */
-    (normalizedTime: number): number;
-    /**
-     * Returns a new polynomial easing with the specified exponent e.
-     * If the exponent is not specified, it defaults to 3, equivalent to cubic.
+     * Restart a timer with the specified callback and optional delay and time.
+     * This is equivalent to stopping this timer and creating a new timer with the specified arguments,
+     * although this timer retains the original invocation priority.
      *
-     * @param e Exponent for polynomial easing.
+     * @param callback A callback function to be invoked and passed in the apparent
+     * elapsed time since the timer became active in milliseconds.
+     * @param delay An optional numeric delay in milliseconds (default = 0) relative to time.
+     * @param time An optional time in milliseconds relative to which the delay is calculated (default = now).
      */
-    exponent(e: number): PolynomialEasingFactory;
+    restart(callbackFn: (elapsed: number) => void, delay?: number, time?: number): void;
+
+    /**
+     * Stop the timer.
+     */
+    stop(): void;
 }
 
 /**
- * Symmetric polynomial easing/easing factory; scales polyIn for t in [0, 0.5] and polyOut for t in [0.5, 1].
- * If the exponent is not specified, it defaults to 3, equivalent to cubic.
- */
-export const easePoly: PolynomialEasingFactory;
-/**
- * Polynomial easing/easing factory; raises t to the specified exponent.
- * If the exponent is not specified, it defaults to 3, equivalent to cubicIn.
- */
-export const easePolyIn: PolynomialEasingFactory;
-
-/**
- * Reverse polynomial easing/easing factory; equivalent to 1 - polyIn(1 - t).
- * If the exponent is not specified, it defaults to 3, equivalent to cubicOut.
- */
-export const easePolyOut: PolynomialEasingFactory;
-
-/**
- * Symmetric polynomial easing/easing factory; scales polyIn for t in [0, 0.5] and polyOut for t in [0.5, 1].
- * If the exponent is not specified, it defaults to 3, equivalent to cubic.
- */
-export const easePolyInOut: PolynomialEasingFactory;
-
-/**
- * Symmetric sinusoidal easing; scales sinIn for t in [0, 0.5] and sinOut for t in [0.5, 1].
+ * Schedules and returns a new timer, invoking the specified callback repeatedly until the timer is stopped.
+ * The callback is passed the (apparent) elapsed time since the timer became active.
  *
- * @param normalizedTime Normalized time typically in the range [0, 1]
+ * @param callback A callback function to be invoked and passed in the apparent
+ * elapsed time since the timer became active in milliseconds.
+ * @param delay An optional numeric delay in milliseconds (default = 0) relative to time.
+ * @param time An optional time in milliseconds relative to which the delay is calculated (default = now).
  */
-export function easeSin(normalizedTime: number): number;
+export function timer(callback: (elapsed: number) => void, delay?: number, time?: number): Timer;
 
 /**
- * Sinusoidal easing; returns sin(t).
+ * Immediately invoke any eligible timer callbacks.
+ */
+export function timerFlush(): void;
+
+/**
+ * Schedules and returns a new timer, invoking the specified callback. The timer is stopped automatically
+ * on its first callback. The callback is passed the (apparent) elapsed time since the timer became active.
  *
- * @param normalizedTime Normalized time typically in the range [0, 1]
+ * @param callback A callback function to be invoked and passed in the apparent
+ * elapsed time since the timer became active in milliseconds.
+ * @param delay An optional numeric delay in milliseconds (default = 0) relative to time.
+ * @param time An optional time in milliseconds relative to which the delay is calculated (default = now).
  */
-export function easeSinIn(normalizedTime: number): number;
+export function timeout(callback: (elapsed: number) => void, delay?: number, time?: number): Timer;
 
 /**
- * Reverse sinusoidal easing; equivalent to 1 - sinIn(1 - t).
+ * Schedules and returns a new timer, invoking the specified callback repeatedly every 'delay' milliseconds
+ * until the timer is stopped.
+ * The callback is passed the (apparent) elapsed time since the timer became active.
  *
- * @param normalizedTime Normalized time typically in the range [0, 1]
+ * @param callback A callback function to be invoked and passed in the apparent
+ * elapsed time since the timer became active in milliseconds.
+ * @param delay An optional numeric delay in milliseconds between repeat invocations of the callback.
+ * If not specified, the interval timer behaves like the regular timer.
+ * @param time An optional time in milliseconds relative to which the initial delay is calculated (default = now).
  */
-export function easeSinOut(normalizedTime: number): number;
-
-/**
- * Symmetric sinusoidal easing; scales sinIn for t in [0, 0.5] and sinOut for t in [0.5, 1].
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeSinInOut(normalizedTime: number): number;
-
-/**
- * Symmetric exponential easing; scales expIn for t in [0, 0.5] and expOut for t in [0.5, 1].
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeExp(normalizedTime: number): number;
-
-/**
- * Exponential easing; raises 2 to the exponent 10 * (t - 1).
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeExpIn(normalizedTime: number): number;
-
-/**
- * Reverse exponential easing; equivalent to 1 - expIn(1 - t).
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeExpOut(normalizedTime: number): number;
-
-/**
- * Symmetric exponential easing; scales expIn for t in [0, 0.5] and expOut for t in [0.5, 1].
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeExpInOut(normalizedTime: number): number;
-
-/**
- * Symmetric circular easing; scales circleIn for t in [0, 0.5] and circleOut for t in [0.5, 1].
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeCircle(normalizedTime: number): number;
-
-/**
- * Circular easing.
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeCircleIn(normalizedTime: number): number;
-
-/**
- * Reverse circular easing; equivalent to 1 - circleIn(1 - t).
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeCircleOut(normalizedTime: number): number;
-
-/**
- * Symmetric circular easing; scales circleIn for t in [0, 0.5] and circleOut for t in [0.5, 1].
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeCircleInOut(normalizedTime: number): number;
-
-/**
- * Reverse bounce easing; equivalent to 1 - bounceIn(1 - t).
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeBounce(normalizedTime: number): number;
-
-/**
- * Bounce easing, like a rubber ball.
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeBounceIn(normalizedTime: number): number;
-
-/**
- * Reverse bounce easing; equivalent to 1 - bounceIn(1 - t).
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeBounceOut(normalizedTime: number): number;
-
-/**
- * Symmetric bounce easing; scales bounceIn for t in [0, 0.5] and bounceOut for t in [0.5, 1].
- *
- * @param normalizedTime Normalized time typically in the range [0, 1]
- */
-export function easeBounceInOut(normalizedTime: number): number;
-
-/**
- * Anticipatory easing function factory
- */
-export interface BackEasingFactory {
-    /**
-     * Calculate eased time.
-     * @param normalizedTime Normalized time typically in the range [0, 1]
-     */
-    (normalizedTime: number): number;
-    /**
-     * Returns a new back easing with the specified overshoot s.
-     * The degree of overshoot is configurable; if not specified, it defaults to 1.70158.
-     *
-     * @param s Overshoot parameter
-     */
-    overshoot(s: number): BackEasingFactory;
-}
-
-/**
- * Symmetric anticipatory easing; scales backIn for t in [0, 0.5] and backOut for t in [0.5, 1].
- * The degree of overshoot is configurable; it not specified, it defaults to 1.70158.
- */
-export const easeBack: BackEasingFactory;
-
-/**
- * Anticipatory easing, like a dancer bending their knees before jumping off the floor.
- * The degree of overshoot is configurable; it not specified, it defaults to 1.70158.
- */
-export const easeBackIn: BackEasingFactory;
-
-/**
- * Reverse anticipatory easing; equivalent to 1 - backIn(1 - t).
- * The degree of overshoot is configurable; it not specified, it defaults to 1.70158.
- */
-export const easeBackOut: BackEasingFactory;
-
-/**
- * Symmetric anticipatory easing; scales backIn for t in [0, 0.5] and backOut for t in [0.5, 1].
- * The degree of overshoot is configurable; it not specified, it defaults to 1.70158.
- */
-export const easeBackInOut: BackEasingFactory;
-
-/**
- * Elastic easing function factory
- */
-export interface ElasticEasingFactory {
-    /**
-     * Calculate eased time.
-     * @param normalizedTime Normalized time typically in the range [0, 1]
-     */
-    (normalizedTime: number): number;
-    /**
-     * Returns a new elastic easing with the specified amplitude a.
-     * Defaults to 1,if not specified.
-     *
-     * @param a Amplitude for elastic easing.
-     */
-    amplitude(a: number): ElasticEasingFactory;
-    /**
-     * Returns a new elastic easing with the specified amplitude a.
-     * Defaults to 0.3,if not specified.
-     *
-     * @param p Period for elastic easing.
-     */
-    period(p: number): ElasticEasingFactory;
-}
-
-/**
- * Reverse elastic easing; equivalent to 1 - elasticIn(1 - t).
- * The amplitude and period of the oscillation are configurable;
- * if not specified, they default to 1 and 0.3, respectively.
- */
-export const easeElastic: ElasticEasingFactory;
-
-/**
- * Elastic easing, like a rubber band.
- * The amplitude and period of the oscillation are configurable;
- * if not specified, they default to 1 and 0.3, respectively.
- */
-export const easeElasticIn: ElasticEasingFactory;
-
-/**
- * Reverse elastic easing; equivalent to 1 - elasticIn(1 - t).
- * The amplitude and period of the oscillation are configurable;
- * if not specified, they default to 1 and 0.3, respectively.
- */
-export const easeElasticOut: ElasticEasingFactory;
-
-/**
- * Symmetric elastic easing; scales elasticIn for t in [0, 0.5] and elasticOut for t in [0.5, 1].
- * The amplitude and period of the oscillation are configurable;
- * if not specified, they default to 1 and 0.3, respectively.
- */
-export const easeElasticInOut: ElasticEasingFactory;
+export function interval(callback: (elapsed: number) => void, delay?: number, time?: number): Timer;
