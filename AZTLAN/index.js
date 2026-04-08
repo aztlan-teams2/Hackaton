27e7d784ea44b01f6a63e4a48162553e0de9cf1f
aztlan-teams2 <waachma28254first@gmail.com>
2026-04-08 15:56:27 -0600

    Add files via upload

--- AZTLAN/index.js
+++ AZTLAN/index.js
@@ -1,53 +1,155 @@
-export {default as arc} from "./arc.js";
-export {default as area} from "./area.js";
-export {default as line} from "./line.js";
-export {default as pie} from "./pie.js";
-export {default as areaRadial, default as radialArea} from "./areaRadial.js"; // Note: radialArea is deprecated!
-export {default as lineRadial, default as radialLine} from "./lineRadial.js"; // Note: radialLine is deprecated!
-export {default as pointRadial} from "./pointRadial.js";
-export {link, linkHorizontal, linkVertical, linkRadial} from "./link.js";
+'use strict';
 
-export {default as symbol, symbolsStroke, symbolsFill, symbolsFill as symbols} from "./symbol.js";
-export {default as symbolAsterisk} from "./symbol/asterisk.js";
-export {default as symbolCircle} from "./symbol/circle.js";
-export {default as symbolCross} from "./symbol/cross.js";
-export {default as symbolDiamond} from "./symbol/diamond.js";
-export {default as symbolDiamond2} from "./symbol/diamond2.js";
-export {default as symbolPlus} from "./symbol/plus.js";
-export {default as symbolSquare} from "./symbol/square.js";
-export {default as symbolSquare2} from "./symbol/square2.js";
-export {default as symbolStar} from "./symbol/star.js";
-export {default as symbolTriangle} from "./symbol/triangle.js";
-export {default as symbolTriangle2} from "./symbol/triangle2.js";
-export {default as symbolWye} from "./symbol/wye.js";
-export {default as symbolTimes, default as symbolX} from "./symbol/times.js";
+/* (c) 2015 Ari Porad (@ariporad) <http://ariporad.com>. License: ariporad.mit-license.org */
+const BuiltinModule = require('module');
+const path = require('path');
 
-export {default as curveBasisClosed} from "./curve/basisClosed.js";
-export {default as curveBasisOpen} from "./curve/basisOpen.js";
-export {default as curveBasis} from "./curve/basis.js";
-export {bumpX as curveBumpX, bumpY as curveBumpY} from "./curve/bump.js";
-export {default as curveBundle} from "./curve/bundle.js";
-export {default as curveCardinalClosed} from "./curve/cardinalClosed.js";
-export {default as curveCardinalOpen} from "./curve/cardinalOpen.js";
-export {default as curveCardinal} from "./curve/cardinal.js";
-export {default as curveCatmullRomClosed} from "./curve/catmullRomClosed.js";
-export {default as curveCatmullRomOpen} from "./curve/catmullRomOpen.js";
-export {default as curveCatmullRom} from "./curve/catmullRom.js";
-export {default as curveLinearClosed} from "./curve/linearClosed.js";
-export {default as curveLinear} from "./curve/linear.js";
-export {monotoneX as curveMonotoneX, monotoneY as curveMonotoneY} from "./curve/monotone.js";
-export {default as curveNatural} from "./curve/natural.js";
-export {default as curveStep, stepAfter as curveStepAfter, stepBefore as curveStepBefore} from "./curve/step.js";
+const nodeModulesRegex = /^(?:.*[\\/])?node_modules(?:[\\/].*)?$/;
+// Guard against poorly-mocked module constructors.
+const Module =
+  module.constructor.length > 1 ? module.constructor : BuiltinModule;
 
-export {default as stack} from "./stack.js";
-export {default as stackOffsetExpand} from "./offset/expand.js";
-export {default as stackOffsetDiverging} from "./offset/diverging.js";
-export {default as stackOffsetNone} from "./offset/none.js";
-export {default as stackOffsetSilhouette} from "./offset/silhouette.js";
-export {default as stackOffsetWiggle} from "./offset/wiggle.js";
-export {default as stackOrderAppearance} from "./order/appearance.js";
-export {default as stackOrderAscending} from "./order/ascending.js";
-export {default as stackOrderDescending} from "./order/descending.js";
-export {default as stackOrderInsideOut} from "./order/insideOut.js";
-export {default as stackOrderNone} from "./order/none.js";
-export {default as stackOrderReverse} from "./order/reverse.js";
+const HOOK_RETURNED_NOTHING_ERROR_MESSAGE =
+  '[Pirates] A hook returned a non-string, or nothing at all! This is a' +
+  ' violation of intergalactic law!\n' +
+  '--------------------\n' +
+  'If you have no idea what this means or what Pirates is, let me explain: ' +
+  'Pirates is a module that makes it easy to implement require hooks. One of' +
+  " the require hooks you're using uses it. One of these require hooks" +
+  " didn't return anything from it's handler, so we don't know what to" +
+  ' do. You might want to debug this.';
+
+/**
+ * @param {string} filename The filename to check.
+ * @param {string[]} exts The extensions to hook. Should start with '.' (ex. ['.js']).
+ * @param {Matcher|null} matcher A matcher function, will be called with path to a file. Should return truthy if the file should be hooked, falsy otherwise.
+ * @param {boolean} ignoreNodeModules Auto-ignore node_modules. Independent of any matcher.
+ */
+function shouldCompile(filename, exts, matcher, ignoreNodeModules) {
+  if (typeof filename !== 'string') {
+    return false;
+  }
+  if (exts.indexOf(path.extname(filename)) === -1) {
+    return false;
+  }
+
+  const resolvedFilename = path.resolve(filename);
+
+  if (ignoreNodeModules && nodeModulesRegex.test(resolvedFilename)) {
+    return false;
+  }
+  if (matcher && typeof matcher === 'function') {
+    return !!matcher(resolvedFilename);
+  }
+
+  return true;
+}
+
+/**
+ * @callback Hook The hook. Accepts the code of the module and the filename.
+ * @param {string} code
+ * @param {string} filename
+ * @returns {string}
+ */
+/**
+ * @callback Matcher A matcher function, will be called with path to a file.
+ *
+ * Should return truthy if the file should be hooked, falsy otherwise.
+ * @param {string} path
+ * @returns {boolean}
+ */
+/**
+ * @callback RevertFunction Reverts the hook when called.
+ * @returns {void}
+ */
+/**
+ * @typedef {object} Options
+ * @property {Matcher|null} [matcher=null] A matcher function, will be called with path to a file.
+ *
+ * Should return truthy if the file should be hooked, falsy otherwise.
+ *
+ * @property {string[]} [extensions=['.js']] The extensions to hook. Should start with '.' (ex. ['.js']).
+ * @property {string[]} [exts=['.js']] The extensions to hook. Should start with '.' (ex. ['.js']).
+ *
+ * @property {string[]} [extension=['.js']] The extensions to hook. Should start with '.' (ex. ['.js']).
+ * @property {string[]} [ext=['.js']] The extensions to hook. Should start with '.' (ex. ['.js']).
+ *
+ * @property {boolean} [ignoreNodeModules=true] Auto-ignore node_modules. Independent of any matcher.
+ */
+
+/**
+ * Add a require hook.
+ *
+ * @param {Hook} hook The hook. Accepts the code of the module and the filename. Required.
+ * @param {Options} [opts] Options
+ * @returns {RevertFunction} The `revert` function. Reverts the hook when called.
+ */
+function addHook(hook, opts = {}) {
+  let reverted = false;
+  const loaders = [];
+  const oldLoaders = [];
+  let exts;
+
+  // We need to do this to fix #15. Basically, if you use a non-standard extension (ie. .jsx), then
+  // We modify the .js loader, then use the modified .js loader for as the base for .jsx.
+  // This prevents that.
+  const originalJSLoader = Module._extensions['.js'];
+
+  const matcher = opts.matcher || null;
+  const ignoreNodeModules = opts.ignoreNodeModules !== false;
+  exts = opts.extensions || opts.exts || opts.extension || opts.ext || ['.js'];
+  if (!Array.isArray(exts)) {
+    exts = [exts];
+  }
+
+  exts.forEach((ext) => {
+    if (typeof ext !== 'string') {
+      throw new TypeError(`Invalid Extension: ${ext}`);
+    }
+    const oldLoader = Module._extensions[ext] || originalJSLoader;
+    oldLoaders[ext] = Module._extensions[ext];
+
+    loaders[ext] = Module._extensions[ext] = function newLoader(mod, filename) {
+      let compile;
+      if (!reverted) {
+        if (shouldCompile(filename, exts, matcher, ignoreNodeModules)) {
+          compile = mod._compile;
+          mod._compile = function _compile(code) {
+            // reset the compile immediately as otherwise we end up having the
+            // compile function being changed even though this loader might be reverted
+            // Not reverting it here leads to long useless compile chains when doing
+            // addHook -> revert -> addHook -> revert -> ...
+            // The compile function is also anyway created new when the loader is called a second time.
+            mod._compile = compile;
+            const newCode = hook(code, filename);
+            if (typeof newCode !== 'string') {
+              throw new Error(HOOK_RETURNED_NOTHING_ERROR_MESSAGE);
+            }
+
+            return mod._compile(newCode, filename);
+          };
+        }
+      }
+
+      oldLoader(mod, filename);
+    };
+  });
+  return function revert() {
+    if (reverted) return;
+    reverted = true;
+
+    exts.forEach((ext) => {
+      // if the current loader for the extension is our loader then unregister it and set the oldLoader again
+      // if not we cannot do anything as we cannot remove a loader from within the loader-chain
+      if (Module._extensions[ext] === loaders[ext]) {
+        if (!oldLoaders[ext]) {
+          delete Module._extensions[ext];
+        } else {
+          Module._extensions[ext] = oldLoaders[ext];
+        }
+      }
+    });
+  };
+}
+
+exports.addHook = addHook;
