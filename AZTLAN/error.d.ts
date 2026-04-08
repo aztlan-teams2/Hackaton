fc3854aeed7bad9da5cfbe9d850d1384e5835431
aztlan-teams <techkalli28255first@gmail.com>
2026-04-08 17:53:29 -0600

    Add files via upload

--- AZTLAN/error.d.ts
+++ AZTLAN/error.d.ts
@@ -1,1 +1,7 @@
-export * from './dist/error';
+declare class BrowserslistError extends Error {
+  constructor(message: any)
+  name: 'BrowserslistError'
+  browserslist: true
+}
+
+export = BrowserslistError
--- AZTLAN/index.js
+++ AZTLAN/index.js
@@ -1,7 +1,2 @@
 'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./cjs/react-is.production.js');
-} else {
-  module.exports = require('./cjs/react-is.development.js');
-}
+module.exports = /^#!(.*)/;
